c7607cc778cdea05e4852cf5c4aec0003afc8b6e
quentinjohnson <70232631+quentinjohnson@users.noreply.github.com>
2020-09-27 14:47:00 -0700

    Update Morty.cpp

--- src/Morty.cpp
+++ src/Morty.cpp
@@ -9,13 +9,133 @@
 
 // Include the Morty header file
 #include "Morty.hpp"
-
-// =========================================================================== // 
-//          Define functions enclosed in Namespace C137 here 
-// =========================================================================== // 
+#include <iostream>;
 
 
+namespace C137 {
 
-// =========================================================================== // 
-//          Define functions enclosed in Namespace Z286 here 
-// =========================================================================== // 
+	void Morty(int start, int stop) {
+													//print Aww, Geez, or Aww Geez Man based on loop invariant
+		std::cout << "Morty C137 says: " << std::endl;
+		
+		for (int i = start; i <= stop; i++) {
+
+			if ((i % 15) == 0) {
+
+			std::cout << i << ": Aww Geez Man" << std::endl;
+			}
+
+			else if ((i % 3) == 0) {
+
+				std::cout << i << ": Aww" << std::endl;
+			}
+
+			else if ((i % 5) == 0) {
+
+				std::cout << i << ": Geez" << std::endl;
+			}
+
+			else {
+				std::cout << i << ":" << std::endl;
+			}
+
+		}
+
+	}
+
+	void MortyStep(int start, int stop, int step) {
+
+		std::cout << "Morty C137 says: " << std::endl;
+
+		for (int i = start; i <= stop; i+= step) {
+
+			if ((i % 15) == 0) {
+
+				std::cout << i << ": Aww Geez Man" << std::endl;
+			}
+
+			else if ((i % 3) == 0) {
+
+				std::cout << i << ": Aww" << std::endl;
+			}
+
+			else if ((i % 5) == 0) {
+
+				std::cout << i << ": Geez" << std::endl;
+			}
+
+			else {
+				std::cout << i << ":" << std::endl;
+			}
+			
+
+		}
+
+	}
+
+}
+
+
+namespace Z286 {
+
+	void Morty(int start, int stop) {
+													//print wwA, zeeG, or wwA zeeG naM based on loop invariant
+
+		std::cout << "Morty Z286 says: " << std::endl;
+
+		for (int i = start; i <= stop; i++) {
+
+			if ((i % 15) == 0) {
+
+				std::cout << i << ": wwA zeeG naM" << std::endl;
+			}
+
+			else if ((i % 3) == 0) {
+
+				std::cout << i << ": wwA" << std::endl;
+			}
+
+			else if ((i % 5) == 0) {
+
+				std::cout << i << ": zeeG" << std::endl;
+			}
+
+			else {
+				std::cout << i << ":" << std::endl;
+			}
+
+		}
+
+	}
+
+	void MortyStep(int start, int stop, int step) {
+
+		std::cout << "Morty Z286 says: " << std::endl;
+
+		for (int i = start; i <= stop; i += step) {
+
+			if ((i % 15) == 0) {
+
+				std::cout << i << ": wwA zeeG naM" << std::endl;
+			}
+
+			else if ((i % 3) == 0) {
+
+				std::cout << i << ": wwA" << std::endl;
+			}
+
+			else if ((i % 5) == 0) {
+
+				std::cout << i << ": zeeG" << std::endl;
+			}
+
+			else {
+				std::cout << i << ":" << std::endl;
+			}
+
+
+		}
+
+	}
+
+}
