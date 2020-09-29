7be055055518ff0b3e14104e22d564534a3a83e7
quentinjohnson <70232631+quentinjohnson@users.noreply.github.com>
2020-09-28 21:37:07 -0700

    Update Morty.cpp

--- src/Morty.cpp
+++ src/Morty.cpp
@@ -9,41 +9,19 @@
 
 // Include the Morty header file
 #include "Morty.hpp"
-#include <iostream>;
+#include <string>
+#include <iostream>
 
 
 namespace C137 {
 
 	void Morty(int start, int stop) {
-													//print Aww, Geez, or Aww Geez Man based on loop invariant
-		std::cout << "Morty C137 says: " << std::endl;
-		
-		for (int i = start; i <= stop; i++) {
 
-			if ((i % 15) == 0) {
-
-			std::cout << i << ": Aww Geez Man" << std::endl;
-			}
-
-			else if ((i % 3) == 0) {
-
-				std::cout << i << ": Aww" << std::endl;
-			}
-
-			else if ((i % 5) == 0) {
-
-				std::cout << i << ": Geez" << std::endl;
-			}
-
-			else {
-				std::cout << i << ":" << std::endl;
-			}
-
-		}
+		Morty(start, stop, 1);
 
 	}
 
-	void MortyStep(int start, int stop, int step) {
+	void Morty(int start, int stop, int step) {
 
 		std::cout << "Morty C137 says: " << std::endl;
 
@@ -79,36 +57,13 @@ namespace C137 {
 namespace Z286 {
 
 	void Morty(int start, int stop) {
-													//print wwA, zeeG, or wwA zeeG naM based on loop invariant
+													//print wwA, zeeG, or naM zeeG wwA based on loop invariant
 
-		std::cout << "Morty Z286 says: " << std::endl;
-
-		for (int i = start; i <= stop; i++) {
-
-			if ((i % 15) == 0) {
-
-				std::cout << i << ": wwA zeeG naM" << std::endl;
-			}
-
-			else if ((i % 3) == 0) {
-
-				std::cout << i << ": wwA" << std::endl;
-			}
-
-			else if ((i % 5) == 0) {
-
-				std::cout << i << ": zeeG" << std::endl;
-			}
-
-			else {
-				std::cout << i << ":" << std::endl;
-			}
-
-		}
+		Morty(start, stop, 1);
 
 	}
 
-	void MortyStep(int start, int stop, int step) {
+	void Morty(int start, int stop, int step) {
 
 		std::cout << "Morty Z286 says: " << std::endl;
 
@@ -116,7 +71,7 @@ namespace Z286 {
 
 			if ((i % 15) == 0) {
 
-				std::cout << i << ": wwA zeeG naM" << std::endl;
+				std::cout << i << ": naM zeeG wwA" << std::endl;
 			}
 
 			else if ((i % 3) == 0) {
